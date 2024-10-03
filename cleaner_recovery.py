f25588b31b0f98d18929ce2b68b85f44dcb48868
Florian Colson <colson.florian51@gmail.com>
2024-10-03 12:11:29 +0200

    11th commit update

--- cleaner_recovery.py
+++ cleaner_recovery.py
@@ -185,6 +185,7 @@ def restore_file(files_to_restore, chosed_disk, chosed_part):
             success_cpt += 1
             pbar.update(1)
 
+        time.sleep(0.25)
         print(f'{success_cpt} fichier(s) restauré(s) avec succès dans le répertoire: {os.getcwd()}\\restored_files')
 
 
